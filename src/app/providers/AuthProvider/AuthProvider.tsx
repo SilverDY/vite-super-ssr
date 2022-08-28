781d67899cd51443dd7d0fad9b0853402b00e3b0
Dmitrii Serebrov <yuserebrov@gmail.com>
2022-08-28 16:52:31 +0300

    fix(auth): fixes user request after login

--- src/app/providers/AuthProvider/AuthProvider.tsx
+++ src/app/providers/AuthProvider/AuthProvider.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 
-import { useSetViewer } from '~entities/viewer';
+import { useSetViewer, useViewer } from '~entities/viewer';
 import { api, createAuthenticatedRequestHandler } from '~shared/api';
 import { useAuthHeader, useIsAuthenticated } from '~shared/lib/auth';
 
@@ -8,18 +8,19 @@ export interface AuthProviderProps extends ComponentWithChildren {}
 
 export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const setViewer = useSetViewer();
+  const viewer = useViewer();
+
   const isAuth = useIsAuthenticated();
   const getToken = useAuthHeader();
-  const token = getToken();
 
-  useMemo(() => {
-    // adds token for axios
-    api.interceptors.request.use(createAuthenticatedRequestHandler(token));
+  const token = useMemo(() => getToken(), [getToken]);
 
-    if (isAuth()) {
+  useEffect(() => {
+    if (!viewer && isAuth()) {
+      api.interceptors.request.use(createAuthenticatedRequestHandler(token));
+
       setViewer();
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token, isAuth]);
 
